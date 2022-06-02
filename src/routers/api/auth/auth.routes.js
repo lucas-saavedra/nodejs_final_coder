f9247e0413355204f4c38369fe45fb4352e0ff16
lucas_saavedra <lucassaavedra50@gmail.com>
2022-06-01 22:58:22 -0300

    Desafio completo | Correcciones Heroku

--- src/routers/api/auth/auth.routes.js
+++ src/routers/api/auth/auth.routes.js
@@ -7,7 +7,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve('./public/uploads'))
+        cb(null, './public/uploads')
     },
     filename: function (req, file, cb) {
         cb(null, uuidv4() + path.extname(file.originalname));
